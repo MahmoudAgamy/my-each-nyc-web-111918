c1ab11e5ffe4ed9f59c85c5b25ffee3eea9c4312
Mahmoud Agamy <agamyv@gmail.com>
2018-11-17 00:58:34 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,13 +1,11 @@
 def my_each(arr)
-  new_arr = []
-  if block_given?
     i = 0
     while i < arr.length
-        new_arr << yield(arr[i])
+         yield(arr[i])
       i+=1
     end
   end
-  return new_arr
+  return arr
 end
 # I don't know how it's passing the test!
 # I should return the new_arr but when I do I've a problem with
