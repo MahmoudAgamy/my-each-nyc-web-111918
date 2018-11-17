b97d69bbee739814b0e659801c6d9b4028fa3e7e
Mahmoud Agamy <agamyv@gmail.com>
2018-11-17 01:03:54 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -6,7 +6,4 @@ def my_each(arr)
     end
   return arr
 end
-# I don't know how it's passing the test!
-# I should return the new_arr but when I do I've a problem with
-  #returned array contains the same elements as the original collection
   
